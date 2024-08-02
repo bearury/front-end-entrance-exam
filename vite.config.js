c52cbaa8a6de73d8bce0dc1eb17cee86c51eb977
webjsmaster <web.js.master@gmail.com>
2024-08-02 22:11:33 +0300

    fix: deploy setting

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	build: {
 		sourcemap: true,
 	},
-	resolve: {
-		alias: {
-			$fonts: resolve('./fonts'),
-		},
-	},
+	base: '/',
 	css: {
 		devSourcemap: true,
 	},
